929618f92a390f6b86a7e0fa2d355c2328b7d478
marioangelozzi <mario.angelozzi@gmail.com>
2021-05-13 10:32:01 -0300

    Heroku model deployment

--- predict_result.py
+++ predict_result.py
@@ -94,5 +94,6 @@ def my_form_post():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5001))
+    # app.run(host='0.0.0.0', port=port)
+    app.run(port=5000,host='0.0.0.0')
